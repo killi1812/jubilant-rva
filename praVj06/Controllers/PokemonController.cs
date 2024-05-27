14dca6cd9a7e9447158d39815afef171f39a2ee9
office10 <office10@ucione.local>
2024-05-27 08:51:26 +0200

    controller

---
+++ praVj06/Controllers/PokemonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace praVj06.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet(Name = "getPokemon")]
+        public IEnumerable<ActionResult> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet(Name = "getPokemonByID")]
+        public IEnumerable<ActionResult> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
