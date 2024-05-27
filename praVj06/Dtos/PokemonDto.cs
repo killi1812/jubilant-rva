51f9b252ec49831ec204fa7ef35d741421aef2a3
killi1812 <cvok.fran@gmail.com>
2024-05-27 08:49:32 +0200

    Pokemon service

---
+++ praVj06/Dtos/PokemonDto.cs
@@ -0,0 +1,10 @@
+﻿namespace praVj06.Dtos
+{
+    public class PokemonDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Level { get; set; }
+    }
+}
